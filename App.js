753468f35e99236e45c0da25734667c0cea1dbda
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2023-08-16 20:39:23 +0200

    Simple base weather UI

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { View, Text, SafeAreaView, StyleSheet } from "react-native"
+import {Feather}from '@expo/vector-icons'
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text>Weather Now</Text>
+    <Feather name="sun" size={100}></Feather>
+    <Text style={styles.temp}>6</Text> 
+    <Text style={styles.feels}>Feels like 5</Text>
+    <View style={styles.highLowWrapper}>
+    <Text style={styles.highLow}>High: 8</Text>
+    <Text style={styles.highLow}>Low: 6</Text>
     </View>
-  );
+    <View style={styles.bodyWrapper}>
+      <Text style={styles.description}>It's Sunny</Text>
+      <Text style={styles.message}>It's a perfect sunny day</Text>
+    </View>
+  </View>
+)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles =StyleSheet.create({
+  container:{
+    backgroundColor:'orange',
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
   },
-});
+  temp:{
+    color:'black',
+    fontSize:48,
+
+  },
+  feels:{
+    fontSize:30,
+    color:'black'
+  },
+  highLow:{
+    fontSize:20,
+    color:'black'
+  },
+  highLowWrapper:{
+    flexDirection:'row'
+  },
+  bodyWrapper:{
+    justifyContent:'flex-end',
+    alignItems:'flex-start',
+  },
+  description:{
+    fontSize:40
+  },
+  message:{
+    fontSize:30
+  }
+})
+export default App
